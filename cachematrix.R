e528ca089e27dab324c5d93eb3d8e9dc627ca6fb
adaycj <amday@wccnet.edu>
2017-04-23 11:52:22 -0400

    Week 3 assignement coursera
    Edited to cashe a matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions first set the value of a matrix and then set the value of a matrix
+## These functions then compute the inverse of a matrix and set that value as well
 
-## Write a short comment describing this function
+## cashe a matrix because this can be a resource intensive process and we don't
+## want to do it over and over 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(cashed_matrix = matrix()) {
+  inverse <- NULL
+  set <- function(internal_function_matrix) {
+    ## modify the variable at the parent level
+    cashed_matrix <<- internal_function_matrix
+    inverse <<- NULL
+  }
+  get <- function() cashed_matrix
+  setinverse <- function(inverse) inverse <<- inverse
+  getinverse <- function() inverse
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cashed_matrix, ...) {
+  ## Return a matrix that is the inverse of 'cashed_matrix'
+  inverse <- cashed_matrix$getinverse()
+  ## if the inverse is not NULL then jsut return the inverse 
+  if(!is.null(inverse)) {
+    message("There is cashed data, so it will be used")
+    ## the return ends the funciton so we skip the inverse work below
+    return(inverse)
+  }
+  data <- cashed_matrix$get()
+  inverse <- solve(data)
+  cashed_matrix$setinverse(inverse)
+  inverse
 }
